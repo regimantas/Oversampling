35a738684d4db333715811d41a6af3d68808924e
Regimantas <regtech0@gmail.com>
2019-12-16 21:01:05 +0200

    Add files via upload

--- src/Oversampling.cpp
+++ src/Oversampling.cpp
@@ -11,16 +11,26 @@ Oversampling::Oversampling(int adcbytes, int samplebytes, int Averaging)
 {
   _samplebytes = constrain(samplebytes, 11, 24);
   _adcbytes = constrain(adcbytes, 10, 12);
+  if (Averaging < 1){
+    Averaging = 1;
+  }
   _Averaging = Averaging;
 }
 
 int Oversampling::read(int pin)
 {
   int SampleCount = _samplebytes - _adcbytes;
-  unsigned long avarage = 0;
+  SampleCount = constrain(SampleCount, 1, 14) ** 4;
+  
+  //unsigned long avarage = 0;
+  unsigned long TotalADC = 0;
   for (int i=0; i<_Averaging; i++){
-    avarage = avarage + analogRead(pin);
+    for (int i=0; i<SampleCount; i++){
+      TotalADC = TotalADC + analogRead(pin);
+    }
   }
+  TotalADC = TotalADC / 2 / _Averaging;
+
   avarage = avarage / _Averaging;
   //_Averaging
   return avarage;
